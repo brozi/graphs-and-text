9539dc91133a6f178ab5e278df30ef6feadacb59
Baptiste <baptiste.roziere@gmail.com>
2016-03-26 19:00:05 +0100

    plein de trucs

--- neural_net.py
+++ neural_net.py
@@ -6,30 +6,39 @@ from sklearn.pipeline import Pipeline
 import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
-path = "validation_set/"
-
-
+validation = False
+path =""
+if(validation):
+    path = "validation_set/"
+else:
+    path = "train_set/"
 
 X_train = pd.read_csv(path+"X_train.csv",index_col=0)
 X_test = pd.read_csv(path+"X_test.csv",index_col=0)
 y_train = pd.Series.from_csv(path+"y_train.csv")
-y_test = pd.Series.from_csv(path+"y_test.csv")
+if(validation):
+    y_test = pd.Series.from_csv(path+"y_test.csv")
 
+#X_train = X_train.drop(["Number of times to cited"], axis = 1)
+#X_test = X_test.drop(["Number of times to cited"], axis = 1)
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.linear_model import LogisticRegression
 #########################################
 scaler1=StandardScaler()
 scaler = MinMaxScaler(feature_range=(0.0, 1.0))
+val_percent = 0.1
 nn = MLP(
 layers=[
-Layer("Rectifier", units=6000),
+    Layer("Rectifier", units=100),
+    Layer("Rectifier", units=100),
+    Layer("Rectifier", units=100),
         Layer("Softmax")],
-        learning_rule = 'momentum', learning_rate=0.01, batch_size = 150,dropout_rate = 0.2,
-        n_iter=20,
+        learning_rule = 'momentum', learning_rate=0.05, batch_size = 100,dropout_rate =0.1,
+        n_iter=100,
         verbose = 1, 
-        valid_size = 0.1, 
-        n_stable = 15,
+        valid_size = val_percent, 
+        n_stable = 30,
         debug = True,
         #    regularize = 'L2'
 )
@@ -44,7 +53,23 @@ clf = Pipeline([
     ('neural network', nn)
     ])
 
-clf.fit(X_train, y_train)
-pred = clf.predict(X_test)
-print accuracy_score(pred, y_test)
+clf.fit(X_train.values, y_train.values)
+pred = clf.predict(X_test.values).flatten()
+if(validation):
+    print "hold out validation:",accuracy_score(pred, y_test)
+if val_percent>0:
+    valid = clf.named_steps["neural network"].valid_set
+    print "nn validation:",accuracy_score(clf.named_steps["neural network"].predict(valid[0]),valid[1][:,1])
+
 #print accuracy_score(clf.predict(X_train),y_train)
+
+def make_submission(predicted_label, name = 'submit.csv'):
+    submit_d = d = {'id' : pd.Series(np.arange(predicted_label.shape[0]).astype(int)),
+                    'category' : pd.Series(predicted_label).astype(int)}
+    submit = pd.DataFrame(submit_d, columns=["id","category"])
+    submit.to_csv(name,index=False)
+    return submit
+
+
+if(not(validation)):
+    submit = make_submission(pred,name = "submit_nn.csv")
